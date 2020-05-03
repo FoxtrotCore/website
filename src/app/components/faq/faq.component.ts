14be6a478b3b60701bc7e8b2d6ca65e27d383188
the-ivo-robotnic <theivorobotnic@gmail.com>
2020-05-02 22:22:30 -0700

    added faq section + reuseable animations

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { AppComponent } from '../../app.component';
+import { AppComponent } from 'src/app/app.component';
+import { FaqQuestion } from 'src/app/models/FaqQuestion';
+import { raw_questions } from 'src/assets/faq';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.css']
 })
+
 export class FaqComponent implements OnInit {
+  questions: FaqQuestion[];
 
   constructor(@Inject(AppComponent) private parent: AppComponent) {
+    this.questions = raw_questions;
     this.parent.page_title = "FAQ";
     this.parent.banner_title = "Frequently Asked Questions";
     this.parent.updatePage();
---
+++ src/app/components/question/question.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, Renderer2, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import { FaqQuestion } from 'src/app/models/FaqQuestion';
+
+function sleep(delay: number) { return new Promise( resolve => setTimeout(resolve, delay) ); }
+
+@Component({
+  selector: 'app-question',
+  templateUrl: './question.component.html',
+  styleUrls: ['./question.component.css']
+})
+
+export class QuestionComponent implements OnInit {
+  collapse: Boolean;
+  @Input() question: String;
+  @Input() short_answer: String;
+  @Input() long_answer: String;
+
+  constructor(private renderer: Renderer2) {}
+
+  @ViewChild('parent', { static: false }) parent;
+  ngOnInit(): void {}
+  ngAfterViewInit(){ this.toggle_collapse(true); }
+
+  toggle_collapse(mode: Boolean = null) {
+    if(mode == null) this.collapse = !this.collapse;
+    else this.collapse = mode;
+
+    var animate = {
+      'true': ['collapse', 'none', '+'],
+      'false': ['expand', 'inline-block', "X"]
+    }[this.collapse.toString()];
+
+    var button = this.parent.nativeElement.querySelector('button');
+    var content = this.parent.nativeElement.querySelector('.content');
+    this.renderer.setStyle(content, 'animation-play-state', 'running');
+    this.renderer.setStyle(content, 'animation-name', animate[0]);
+    button.innerText = animate[2];
+    if(this.collapse) {
+      (async () => {
+        await sleep(100);
+        this.renderer.setStyle(content, 'display', animate[1]);
+      })();
+    }
+    else this.renderer.setStyle(content, 'display', animate[1]);
+  }
+}
