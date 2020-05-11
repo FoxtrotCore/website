7007fd8ca9327899d1c6de0f6fd478294ceaf1bc
the-ivo-robotnic <theivorobotnic@gmail.com>
2020-05-10 22:25:13 -0700

    Setup the test stack + Travis CI.

--- src/app/services/ftf-api.service.spec.ts
+++ src/app/services/ftf-api.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
 import { FtfApiService } from './ftf-api.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 
 describe('FtfApiService', () => {
   let service: FtfApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ HttpClient, HttpHandler ]
+    });
     service = TestBed.inject(FtfApiService);
   });
 
