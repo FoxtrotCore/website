fe789ad781904af7415f603d7e84b25fa7c888fe
dmitri-mcguckin <dmitri.mcguckin26@gmail.com>
2020-06-08 20:22:16 -0700

    Updated FAQ questions.

--- src/assets/faq.ts
+++ src/assets/faq.ts
@@ -12,23 +12,23 @@ export var raw_questions: FaqQuestion[] = [
     long_answer: "We made the original transcripts via some simple typography rules via our [transcription guide](https://foxtrotfanatics.page.link/guide), formatted them to a pre **Aegisub** -form via our [Script Eater Bot](https://github.com/FoxtrotCore/script-eater), and then exported them from Aegisub as .ASS files. We also took the time to convert them to .STI files for YouTube compatibility. All is available via the subtitles link above."
   },
   {
-    question: "Can you upload your work to Pirates-Bay/MegaUpload/etc.?",
+    question: "Can you upload your work to Pirates-Bay/MegaUpload/etc. or provide it to me privately?",
     short_answer: "No.",
-    long_answer: "At the beginning of this project, we set some standards. One of the first was that we are not a piracy gig. We're not looking to replace the official release. _(Which[you should probably support](https://www.youtube.com/channel/UCn9zTBZVl6lu72s-6fXttZg) if you want to incentivize Mediatoon to bring back CL)_. We're just quality snobs looking to make some good software and enjoy a good show along the way. Like we said in the question above, it's easy to trace our remastered work back to us, and we're not looking to get in trouble, legal or otherwise."
+    long_answer: "At the beginning of this project, we set some standards. One of the first was that we are not a piracy gig. We're not looking to replace the official release. _([Which you should probably support](https://www.youtube.com/channel/UCn9zTBZVl6lu72s-6fXttZg) if you want to incentivize Mediatoon to bring back CL)_. We're just quality snobs looking to make some good software and enjoy a good show along the way. Like we said in the question above, it's easy to trace our remastered work back to us, and we're not looking to get in trouble, legal or otherwise."
   },
   {
     question: "Are you going to host any more CL-Community-Rewatches?",
-    short_answer: "No.",
-    long_answer: "We've been doing the community rewatches for two straight years, and it was a blast. **However** , rewatches are hard to schedule, especially when trying to take into account both European and American time zones and the viewership levels don't warrant the time and effort it takes for us to do them."
+    short_answer: "No. But [Project Eradication](https://www.twitch.tv/projecteradication) is doing them again.",
+    long_answer: "We've been doing the community rewatches for two straight years, and it was a blast. **However**, rewatches are hard to schedule, especially when trying to take into account both European and American time zones and the viewership levels don't warrant the time and effort it takes for us to do them. **But** all hope is not lost, if you're looking to hang out with other CL fans and watch the show, [Project Eradication](https://www.twitch.tv/projecteradication) has been doing weekly rewatches over on their Twitch channel. Feel free to bug them in their [Discord](https://discord.gg/e3EGwQA) for schedule info and additional details."
   },
   {
-    question: "Can I organize a CL community rewatches?",
-    short_answer: "You absolutely can.",
-    long_answer: "While FTF itself will not be hosting such events anymore, we'd be more than happy to try and sponsor your event and do our best to give you the resources needed to hold it. What resources we can offer are dependant on what it is you're looking to do, so it would be best to contact us privately and work out what is reasonable. At the minimum, we can offer subtitles as well as some basic setup/configs for [CyTube](https://cytu.be/), a YouTube synchronizing platform we used to use for our own rewatches. We can also offer some tips/tricks from scheduling/modding from our two years of experience. _(Do note that this doesn't inherently include access to our remasters of the show, for most people/groups, we will not provide this)_."
+    question: "Are you going to upscale CL: Evolution?",
+    short_answer: "No.",
+    long_answer: "Evolution is already in HD, so upscaling it wouldn't do much good. On top of that, Evolution is a live-action series, which makes upscaling it, not impossible but an absolute pain in the \\_\\_\\_. More research on better edge-finders operating on non-animated images would need to be done in order to *properly* upscale CL:E. FTU is not currently outfitted to handle such a task and we're not currently in a position to commit to that level of R&D. You are absolutely welcome to take a shot at it for yourself because we've since made [Foxtrot Upscaler](https://github.com/FoxtrotCore/FoxTrotUpscaler) open-source under **GPL 3.0**, you're welcome to use it *as is* or modify it to your own liking."
   },
   {
-    question: "Are you going to upscale anything else? Like CL: Evolution?",
-    short_answer: "No.",
-    long_answer: "Evolution is already in HD, so upscaling it wouldn't do much good. On top of that, Evolution is a live-action series, which makes upscaling it, not impossible but an absolute pain in the ___. Lots more research on better edge-finders for non-animated images would need to be done and FTU is not currently outfitted to handle such a task. You are absolutely welcome to take a shot at it for yourself because we've since made our [Upscaler](https://github.com/FoxtrotCore/FoxTrotUpscaler) open-source under **GPL 3.0** , so you're welcome to use it, _as is_ , or modify it to work with something like CL: Evolution, if +1080p live-action and crapily written story arcs is your thing. _:P_"
+    question: "Are you going to maintain Foxtrot Upscaler?",
+    short_answer: "[FTU](https://github.com/FoxtrotCore/FoxTrotUpscaler) will only receive critical bug fixes or security patches from time to time.",
+    long_answer: "FTU was made with a specific architecture in mind to get around some of the limitations we had back in 2017, (when we were actively developing and using it). We've already added all the features we wanted in order to overcome those limitations and achieve a fully remastered CL. Adding new features or trying to adapt it to support linux would be difficult without simultaneously rewriting a lot of the underlying code. **However**, that doesn't mean we're done making software! On our backlog, we have plans to make **FTUv2** which will be a total rewrite of the upscaler and will aim to fix many of the baked-in problems we had with FTU and perhaps even introduce a few new features. Now is not the time to outline all the fixes and features but we'll be sure to announce it [@foxtrotfanatics](https://twitter.com/foxtrotfanatics) when we're ready to get started. Be sure to drop us a follow over there so you can be in-the-know when we start."
   }
 ];
